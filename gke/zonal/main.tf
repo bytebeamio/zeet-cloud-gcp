71c59971b5e0f97559b4840922cc363c3056501c
shrbb <115218425+shrbb@users.noreply.github.com>
2024-03-27 22:07:43 +0530

    removed default nodes

--- gke/zonal/main.tf
+++ gke/zonal/main.tf
@@ -351,38 +351,6 @@ module "gke" {
       auto_upgrade              = true
       preemptible               = false
       initial_node_count        = 1
-    },
-    {
-      name                      = "e2-standard-2-guara-preemp"
-      machine_type              = "e2-standard-2"
-      node_locations            = var.zone
-      min_count                 = 0
-      max_count                 = 10
-      local_ssd_count           = 0
-      local_ssd_ephemeral_count = 0
-      disk_size_gb              = 100
-      disk_type                 = "pd-standard"
-      image_type                = "COS_CONTAINERD"
-      auto_repair               = true
-      auto_upgrade              = true
-      preemptible               = true
-      initial_node_count        = 0
-    },
-    {
-      name                      = "e2-standard-2-dedicated"
-      machine_type              = "e2-standard-2"
-      node_locations            = var.zone
-      min_count                 = 0
-      max_count                 = 10
-      local_ssd_count           = 0
-      local_ssd_ephemeral_count = 0
-      disk_size_gb              = 100
-      disk_type                 = "pd-standard"
-      image_type                = "COS_CONTAINERD"
-      auto_repair               = true
-      auto_upgrade              = true
-      preemptible               = false
-      initial_node_count        = 0
     }
     ], flatten([for gpu, sizes in local.gpu_instances :
       [for size in sizes : merge({
